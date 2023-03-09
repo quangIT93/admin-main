9e1e8b0bae77cba932528606f9d0976ae39d8f6e
phanthang-creat <phanthang052@gmail.com>
2023-03-09 09:01:13 +0700

    remove chat pages

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,57 +1,57 @@
-import classNames from "classnames/bind";
-import { Button } from "@mui/material";
+// import classNames from "classnames/bind";
+// import { Button } from "@mui/material";
 
-import styles from "./Chat.module.scss";
-import { PopupHeader } from "components";
-import avatar from "data/avatar2.jpg";
-import { useAppStateContext } from "contexts/AppContext";
+// import styles from "./Chat.module.scss";
+// import { PopupHeader } from "components";
+// import avatar from "data/avatar2.jpg";
+// import { useAppStateContext } from "contexts/AppContext";
 
-const cx = classNames.bind(styles);
+// const cx = classNames.bind(styles);
 
-const Chat = () => {
-  const { themeMode, setShowPopupStatus } = useAppStateContext();
+// const Chat = () => {
+//   const { themeMode, setShowPopupStatus } = useAppStateContext();
 
-  return (
-    <div className={cx("wrapper", themeMode === "dark" ? "dark" : "")}>
-      <PopupHeader
-        title="Messages"
-        onClose={() => setShowPopupStatus((prev) => ({ ...prev, chat: false }))}
-      />
+//   return (
+//     <div className={cx("wrapper", themeMode === "dark" ? "dark" : "")}>
+//       <PopupHeader
+//         title="Messages"
+//         onClose={() => setShowPopupStatus((prev) => ({ ...prev, chat: false }))}
+//       />
 
-      <div className={cx("item", themeMode === "dark" ? "dark" : "")}>
-        <img src={avatar} alt="" />
-        <div className={cx("content")}>
-          <p className={cx("title")}>Roman Joined the Team!</p>
-          <p className={cx("sub-title")}>Congratulate him</p>
-          <p className={cx("time")}>9:08 AM</p>
-        </div>
-      </div>
+//       <div className={cx("item", themeMode === "dark" ? "dark" : "")}>
+//         <img src={avatar} alt="" />
+//         <div className={cx("content")}>
+//           <p className={cx("title")}>Roman Joined the Team!</p>
+//           <p className={cx("sub-title")}>Congratulate him</p>
+//           <p className={cx("time")}>9:08 AM</p>
+//         </div>
+//       </div>
 
-      <div className={cx("item", themeMode === "dark" ? "dark" : "")}>
-        <img src={avatar} alt="" />
-        <div className={cx("content")}>
-          <p className={cx("title")}>Roman Joined the Team!</p>
-          <p className={cx("sub-title")}>Congratulate him</p>
-          <p className={cx("time")}>9:08 AM</p>
-        </div>
-      </div>
+//       <div className={cx("item", themeMode === "dark" ? "dark" : "")}>
+//         <img src={avatar} alt="" />
+//         <div className={cx("content")}>
+//           <p className={cx("title")}>Roman Joined the Team!</p>
+//           <p className={cx("sub-title")}>Congratulate him</p>
+//           <p className={cx("time")}>9:08 AM</p>
+//         </div>
+//       </div>
 
-      <div className={cx("item", themeMode === "dark" ? "dark" : "")}>
-        <img src={avatar} alt="" />
-        <div className={cx("content")}>
-          <p className={cx("title")}>Roman Joined the Team!</p>
-          <p className={cx("sub-title")}>Congratulate him</p>
-          <p className={cx("time")}>9:08 AM</p>
-        </div>
-      </div>
+//       <div className={cx("item", themeMode === "dark" ? "dark" : "")}>
+//         <img src={avatar} alt="" />
+//         <div className={cx("content")}>
+//           <p className={cx("title")}>Roman Joined the Team!</p>
+//           <p className={cx("sub-title")}>Congratulate him</p>
+//           <p className={cx("time")}>9:08 AM</p>
+//         </div>
+//       </div>
 
-      <div className={cx("button")}>
-        <Button fill fullwidth>
-          See all messages
-        </Button>
-      </div>
-    </div>
-  );
-};
+//       <div className={cx("button")}>
+//         <Button fill fullwidth>
+//           See all messages
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Chat;
+// export default Chat;
