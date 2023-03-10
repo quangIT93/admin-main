843910a35d0bd122aad88e48c13c43b810fd509b
thangsuperman <thangjenny2002@gmail.com>
2023-03-10 14:02:56 +0700

    fix: fixed the weird text appear

--- src/index.js
+++ src/index.js
@@ -5,16 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-=======
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);  
->>>>>>> origin/main
