a2de44ccc6f10d06ed329dd555482de4ddaf32c2
thangsuperman <thangjenny2002@gmail.com>
2023-03-22 19:29:24 +0700

    feat: changed a little logic of textfield search post by location in Theme page

--- src/components/Dialog/SelectThemePosts.jsx
+++ src/components/Dialog/SelectThemePosts.jsx
@@ -1,6 +1,12 @@
 import React, { useState, memo } from "react";
+import { styled } from "@mui/material/styles";
 import { axios } from "configs";
 import {
+  Grid,
+  Box,
+  MenuItem,
+  TextField,
+  FormControlLabel,
   DialogContent,
   DialogTitle,
   DialogActions,
@@ -13,6 +19,13 @@ import { Table, Dialog } from "components";
 import { postListColumns } from "configs/table";
 import { fetchAllLocations } from "#api";
 import "./SelectThemePosts.scss";
+import { useSubmit } from "react-router-dom";
+
+const Item = styled(Box)(({ theme }) => ({
+  textarea: {
+    fontSize: "1rem",
+  },
+}));
 
 const SelectThemePostsDialog = ({
   themeId,
@@ -48,78 +61,197 @@ const SelectThemePostsDialog = ({
     }
   };
 
+  // function Dropdown(props) {
+  //   const { locations, wards, districts, setWards, setDistricts } = props;
+
+  //   const handleOnChangeProvince = (e) => {
+  //     const selectedProvinceName = e.target.value;
+  //     const province = locations.find(
+  //       (location) => location.province_name === selectedProvinceName
+  //     );
+  //     if (province) {
+  //       // Reset the districts and wards based on the selected province
+  //       setDistricts(province.districts);
+  //       setWards(province.districts[0].wards);
+  //     }
+  //   };
+
+  //   const handleOnChangeDistrict = (e) => {
+  //     const selectedDistrict = e.target.value;
+  //     const newDistrict = districts.find(
+  //       (district) => district.district === selectedDistrict
+  //     );
+  //     const wards = newDistrict.wards;
+  //     setWards(wards);
+  //   };
+
+  //   const handleOnChangeWard = (e) => {};
+
+  //   React.useEffect(() => {
+  //     setDistricts(locations[0].districts);
+  //     setWards(locations[0].districts[0].wards);
+  //   }, []);
+
+  //   return (
+  //     <div className="dropdown">
+  //       <div className="dropdown__item">
+  //         <div>Tỉnh/Thành phố</div>
+  //         <select onChange={handleOnChangeProvince}>
+  //           {locations
+  //             ? locations.map((province) => (
+  //                 <option key={province.province_id}>
+  //                   {province.province_name}
+  //                 </option>
+  //               ))
+  //             : undefined}
+  //         </select>
+  //         <div className="dropdown__item">
+  //           <div>Quận/Huyện</div>
+  //           <select onChange={handleOnChangeDistrict}>
+  //             {districts
+  //               ? districts.map((district) => (
+  //                   <option key={district.district_id}>
+  //                     {district.district}
+  //                   </option>
+  //                 ))
+  //               : undefined}
+  //           </select>
+  //         </div>
+  //         <div className="dropdown__item">
+  //           <div>Phường/Xã</div>
+  //           <select onChange={handleOnChangeWard}>
+  //             {wards
+  //               ? wards.map((ward) => (
+  //                   <option key={ward.id}>{ward.full_name}</option>
+  //                 ))
+  //               : undefined}
+  //           </select>
+  //         </div>
+  //       </div>
+  //       <div className="dropdown__item">
+  //         <div>Phường/Xã</div>
+  //         <input type="text" />
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
   function Dropdown(props) {
     const { locations, wards, districts, setWards, setDistricts } = props;
+    const [provinceId, setProvinceId] = React.useState(-1);
+    const [districtId, setDistrictId] = React.useState(-1);
+    const [wardId, setWardId] = React.useState(-1);
+
+    console.log("locations :>> ", locations);
 
     const handleOnChangeProvince = (e) => {
-      const selectedProvinceName = e.target.value;
+      setProvinceId((prev) => prev + 1);
+      console.log("handleOnChangeProvince");
+      const selectedProvinceId = e.target.value;
+      console.log("selectedProvinceId :>> ", e.target.value);
       const province = locations.find(
-        (location) => location.province_name === selectedProvinceName
+        (location) => location.province_id === selectedProvinceId
       );
+
       if (province) {
         // Reset the districts and wards based on the selected province
         setDistricts(province.districts);
         setWards(province.districts[0].wards);
       }
+
+      console.log("Province selected :>> ", province);
     };
 
     const handleOnChangeDistrict = (e) => {
-      const selectedDistrict = e.target.value;
+      console.log("handleOnChangeDistrict");
+      const selectedDistrictId = e.target.value;
       const newDistrict = districts.find(
-        (district) => district.district === selectedDistrict
+        (district) => district.district_id === selectedDistrictId
       );
       const wards = newDistrict.wards;
       setWards(wards);
     };
 
-    const handleOnChangeWard = (e) => {};
+    const handleOnChangeWard = (e) => {
+      console.log("handleOnChangeWard");
+      const selectedWardId = e.target.value;
+      console.log("selectedWardId :>> ", selectedWardId);
+      setWardId(selectedWardId);
+    };
 
     React.useEffect(() => {
       setDistricts(locations[0].districts);
       setWards(locations[0].districts[0].wards);
     }, []);
 
+    console.log("Dropdown just rendered");
+
     return (
-      <div className="dropdown">
-        <div className="dropdown__item">
-          <div>Tỉnh/Thành phố</div>
-          <select onChange={handleOnChangeProvince}>
-            {locations
-              ? locations.map((province) => (
-                  <option key={province.province_id}>
-                    {province.province_name}
-                  </option>
-                ))
-              : undefined}
-          </select>
-          <div className="dropdown__item">
-            <div>Quận/Huyện</div>
-            <select onChange={handleOnChangeDistrict}>
-              {districts
-                ? districts.map((district) => (
-                    <option key={district.district_id}>
-                      {district.district}
-                    </option>
-                  ))
-                : undefined}
-            </select>
-          </div>
-          <div className="dropdown__item">
-            <div>Phường/Xã</div>
-            <select onChange={handleOnChangeWard}>
-              {wards
-                ? wards.map((ward) => (
-                    <option key={ward.id}>{ward.full_name}</option>
-                  ))
-                : undefined}
-            </select>
-          </div>
-        </div>
-        <div className="dropdown__item">
-          <div>Phường/Xã</div>
-          <input type="text" />
-        </div>
-      </div>
+      <Grid container xs={12} spacing={4}>
+        <Grid item lg={4}>
+          <TextField
+            label="Tỉnh/Thành phố"
+            variant="outlined"
+            // onChange={() => console.log("On change text field")}
+            // value={provinceId || ""}
+            onChange={handleOnChangeProvince}
+            fullWidth
+            select
+          >
+            {locations.map((location) => (
+              <MenuItem key={location.province_id} value={location.province_id}>
+                {location.province_name}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+
+        {/* District */}
+        <Grid item lg={4}>
+          <Item>
+            <TextField
+              label="Quận/Huyện"
+              variant="outlined"
+              // value={
+              //   post.districtId ||
+              //   (districts.length > 0 ? districts[0].district_id : "")
+              // }
+              onChange={handleOnChangeDistrict}
+              fullWidth
+              select
+            >
+              {districts.map((district) => (
+                <MenuItem
+                  key={district.district_id}
+                  value={district.district_id}
+                >
+                  {district.district}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Item>
+        </Grid>
+
+        {/* Ward */}
+        <Grid item lg={4}>
+          <TextField
+            label="Phường/Xã"
+            variant="outlined"
+            // value="hello"
+            // onChange={() => console.log("On change text field")}
+            // value={post.provinceId || ""}
+            onChange={handleOnChangeWard}
+            fullWidth
+            select
+          >
+            {wards.map((ward) => (
+              <MenuItem key={ward.id} value={ward.id}>
+                {ward.full_name}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+      </Grid>
     );
   }
 
@@ -128,12 +260,15 @@ const SelectThemePostsDialog = ({
     const [locations, setLocation] = React.useState([]);
     const [districts, setDistricts] = React.useState([]);
     const [wards, setWards] = React.useState([]);
+
     const getLocation = async () => {
       const location = await fetchAllLocations();
       setLocation(location);
       setDistricts([]);
       setWards([]);
     };
+
+    console.log("Just rendered the SearchByLocation");
 
     React.useEffect(() => {
       getLocation();
@@ -148,7 +283,7 @@ const SelectThemePostsDialog = ({
           Tìm kiếm bằng địa điểm
         </button>
         {isActive && (
-          <div className="location__dropdown">
+          <div style={{ width: "100%" }} className="location__dropdown">
             <Dropdown
               locations={locations}
               wards={wards}
@@ -162,6 +297,44 @@ const SelectThemePostsDialog = ({
     );
   }
 
+  // function SearchByLocation() {
+  //   const [isActive, setIsActive] = React.useState(false);
+  //   const [locations, setLocation] = React.useState([]);
+  //   const [districts, setDistricts] = React.useState([]);
+  //   const [wards, setWards] = React.useState([]);
+  //   const getLocation = async () => {
+  //     const location = await fetchAllLocations();
+  //     setLocation(location);
+  //     setDistricts([]);
+  //     setWards([]);
+  //   };
+
+  //   React.useEffect(() => {
+  //     getLocation();
+  //   }, []);
+
+  //   return (
+  //     <div className="search-bylocation">
+  //       <button
+  //         className="location__button"
+  //         onClick={() => setIsActive((prev) => !prev)}
+  //       >
+  //         Tìm kiếm bằng địa điểm
+  //       </button>
+  //       {isActive && (
+  //         <div className="location__dropdown">
+  //           <Dropdown
+  //             locations={locations}
+  //             wards={wards}
+  //             districts={districts}
+  //             setWards={setWards}
+  //             setDistricts={setDistricts}
+  //           />
+  //         </div>
+  //       )}
+  //     </div>
+  //   );
+  // }
   React.useEffect(() => {
     setPostIdsSelection(postsOfTheme.map((post) => post.id));
   }, [postsOfTheme]);
@@ -169,7 +342,7 @@ const SelectThemePostsDialog = ({
   return (
     <>
       <Dialog disableEscapeKeyDown open={open} onClose={() => setOpen(false)}>
-        <div className="select-post">
+        <div style={{ padding: "1rem" }} className="">
           <DialogTitle variant="h4" sx={{ padding: "20px 24px" }}>
             Select posts
           </DialogTitle>
