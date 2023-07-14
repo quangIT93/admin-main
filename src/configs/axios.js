05f394623add2561223313d9150017741e0a0209
phanthang-creat <phanthang052@gmail.com>
2023-07-14 09:40:19 +0700

    modify neoworks

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create instance
 const instance = axios.create({
-  // baseURL: "https://neoworks.vn/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://neoworks.vn/api",
+  // baseURL: "http://localhost:5000/api",
   timeout: 10000,
   headers: {
       "Content-Type": "application/json",
